0271cfadd0e7741fe48e63f5bdae1d77ae734ab4
Ouki76 <84971861+OukiLove@users.noreply.github.com>
2022-10-11 15:44:16 +0600

    Update

--- Loader/Loader/Auth/auth.cpp
+++ Loader/Loader/Auth/auth.cpp
@@ -1,11 +1,11 @@
 #include "auth.hpp"
 
 std::string hwid[5] = {
-	"{3a164f40-27c3-11ed-bec6-806e6f6e6963}", // Lamborghini#0660
-	"{2ebf4d0a-1369-11ed-b50d-806e6f6e6963}", // .1#7161
-	"{71d5df84-2096-11ed-b8e8-806e6f6e6963}", // Ouki76
-	"{33619c15-691d-11ea-b2b0-806e6f6e6963}", // 978M02V34987M#1553
-	"{8e076033-0397-11ed-a54b-806e6f6e6963}"
+	"",
+	"",
+	"",
+	"",
+	""
 };
 
 std::string auth::getHWID() {
@@ -19,7 +19,7 @@ std::string auth::getHWID() {
 
 bool auth::GetAuth(std::string nowHWID)
 {
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < hwid->length(); i++)
 	{
 		if (nowHWID == hwid[i])
 			return true;
